839412d320f2a765c7276a1f4c331f3c715359ab
ronaldmark.antonio <ronaldmark.antonio@gmail.com>
2025-08-20 23:17:29 +0800

    Add capstone2 code for s51

---
+++ README.md
@@ -0,0 +1,25 @@
+Capstone 2 Demo App Overview:
+Application Name: FusionTechPro (Gadget E-Commerce API)
+
+Team Members:
+1.Jerry Señas
+2.Ronald Mark Antonio 
+
+User Credentials:
+1.Admin User
+email: admin@mail.com
+password: admin123
+2.Dummy Customer
+email: jamesDoe@mail.com
+password: sample123
+
+Features by Student-1
+User Resources:
+1.Retrieve User Details
+2.Set user as admin(Admin only)
+3.Update Password
+
+Features by Student-2
+User Resources:
+1.User registration
+2.User authentication
--- auth.js
+++ auth.js
@@ -1,9 +1,6 @@
 const jwt = require("jsonwebtoken");
-const JWT_SECRET_KEY="CourseBookingAPI"
-require('dotenv').config();
-
-//[SECTION] Token Creation
-
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 
 module.exports.createAccessToken = (user) => {
     const data = {
@@ -15,75 +12,50 @@ module.exports.createAccessToken = (user) => {
     return jwt.sign(data, JWT_SECRET_KEY, {});
 }
 
-//[SECTION] Token Verification
-
 module.exports.verify = (req, res, next) => {
-    console.log(req.headers.authorization);
-
     let token = req.headers.authorization;
 
-    if(typeof token === "undefined"){
-        return res.send({ auth: "Failed. No Token" });
+    if (typeof token === "undefined") {
+        return res.status(401).json({
+            detail: "Authentication credentials were not provided."
+        });
     } else {
-        console.log(token);
-        //Bearer Token ejdlaskfndlskfjlksd
-        token = token.slice(7, token.lenght);
-        console.log(token);
-
-
-        //[SECTION] Token decryption
-
-        jwt.verify(token, JWT_SECRET_KEY, function(err, decodedToken){
-            if(err) {
-                return res.status(403).send({
+        token = token.slice(7, token.length);
+        jwt.verify(token, JWT_SECRET_KEY, function (err, decodedToken) {
+            if (err) {
+                // Zuitt API return status 200... lol?
+                return res.status(403).json({
                     auth: "Failed",
                     message: err.message
                 });
             } else {
-                console.log("Result from verify method:")
-                console.log(decodedToken);
-
                 req.user = decodedToken;
-
                 next();
             }
-        })
-
+        });
     }
 }
-
-
-//[SECTION] Verify Admin
 
 module.exports.verifyAdmin = (req, res, next) => {
     if(req.user.isAdmin) {
         next();
     } else {
-        return res.status(403).send({
+        return res.status(403).json({
             auth: "Failed",
             message: "Action Forbidden"
-        })
+        });
     }
 }
 
-
-// [SECTION] Error Handler
 module.exports.errorHandler = (err, req, res, next) => {
-    // Log the error
     console.error(err);
 
-    //Add status code 500
     const statusCode = err.status || 500;
-    // if the error object contains a message property, we use it as the error message; otherwise, we default to 'Internal Server Error'.
-    // || -> It ensures that default values are used in such cases, providing a fallback mechanism for error handling.
     const errorMessage = err.message || 'Internal Server Error';
 
-    // Send a standardized error response
-    //We construct a standardized error response JSON object with the appropriate error message, status code, error code, and any additional details provided in the error object.
     res.status(statusCode).json({
         error: {
-            message: errorMessage,
-            errorCode: err.code || 'SERVER_ERROR',
+            error: errorMessage,
             details: err.details
         }
     });
