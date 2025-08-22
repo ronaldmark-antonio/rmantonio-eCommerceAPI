58830de01346fc16f297370da41a336d5855f896
jerrysenas1 <jerrysenas1@gmail.com>
2025-08-22 19:22:07 +0800

    Add correct jwt secret key var name

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const JWT_SECRET_KEY = "ECommerceAPI"
+const JWT_SECRET_KEY = process.env.JWT_SECRET
 
 module.exports.createAccessToken = (user) => {
     const data = {
